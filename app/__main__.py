cbf1f729545505d67242bd1d0b64283a54a2f142
Max Malynowsky <github@maxmalynowsky.com>
2026-04-02 17:17:49 +0200

    make edge-matching optional

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,5 @@
+import argparse
+
 from .clean.cod_ab import clean_admin, clean_admin_em
 from .config import data_dir
 from .download.admin0 import download_admin0
@@ -6,11 +8,20 @@ from .edge_extender import edge_extender
 
 def main() -> None:
     """Run main Function."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--edge-match",
+        action="store_true",
+        help="Run edge extender and em cleaning steps",
+    )
+    args = parser.parse_args()
+
     download_admin0()
     for layer in sorted((data_dir / "inputs").glob("*adm*")):
         clean_admin(layer)
-        edge_extender(layer)
-        clean_admin_em(layer)
+        if args.edge_match:
+            edge_extender(layer)
+            clean_admin_em(layer)
 
 
 if __name__ == "__main__":
